6e048e3aa22b8d7feffdd8405ba0d3f7eb22eb69
andresfsh96 <andresfsh96@gmail.com>
2025-10-16 21:36:40 -0500

    Funcionalidad agregada

--- Clase2/Clase2Git/src/App.java
+++ Clase2/Clase2Git/src/App.java
@@ -2,5 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hola Mundo!");
         System.out.println("Modificacion para commit");
+        System.out.println("Modificacion 1 para nueva rama");
+        System.out.println("Funcionalidad agregada en rama nueva-rama");
     }
 }
